f975ed50a24f1150fcd197b2c3cdbd3feef290d9
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-02-25 23:59:32 +0700

    menu

--- src/components/FoodMenu.jsx
+++ src/components/FoodMenu.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import CategoryButtons from "./CategoryButtons";
+import FoodList from "./FoodList";
 function FoodMenu() {
+  const [category, setCategory] = useState("sheap");
   return (
     <div>
-      
+      <CategoryButtons setCategory={setCategory} />
+      <FoodList category={category} />
     </div>
-  )
+  );
 }
 
-export default FoodMenu
+export default FoodMenu;
