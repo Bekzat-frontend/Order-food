b3f5ba28825ffac0ba77e726a96644300ba549ff
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-02-26 00:02:07 +0700

    fadd

--- src/components/CategoryButtons.jsx
+++ src/components/CategoryButtons.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function CategoryButtons() {
+function CategoryButtons({ setCategory }) {
   return (
     <div>
-      
+      <select onChange={(e) => setCategory(e.target.value)}>
+        <option>Арзан тамак</option>
+        <option>Кымбат тамак</option>
+      </select>
     </div>
-  )
+  );
 }
 
-export default CategoryButtons
+export default CategoryButtons;
